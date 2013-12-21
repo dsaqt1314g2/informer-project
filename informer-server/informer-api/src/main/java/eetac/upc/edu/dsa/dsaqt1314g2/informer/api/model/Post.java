22af54f9e8fd29c897f4614485fe01344bd99832
Raul Suarez <raul.suarez.marin@gmail.com>
2013-12-21 13:19:09 +0100

    Modelo post, web.xml y context.xml mejorados

--- informer-server/informer-api/src/main/java/eetac/upc/edu/dsa/dsaqt1314g2/informer/api/model/Post.java
+++ informer-server/informer-api/src/main/java/eetac/upc/edu/dsa/dsaqt1314g2/informer/api/model/Post.java
@@ -1,8 +1,106 @@
 package eetac.upc.edu.dsa.dsaqt1314g2.informer.api.model;
 
+import java.util.Date;
 
 public class Post {
-	
-	
-	
+	private int identificador;
+	private int id_user;
+	private int visibilidad; //0=anonimo, 1=nombre para amigos, 2=publico, 3=no visible, 5=pendiente de moderar
+	private String asunto;
+	private String contenido;
+	private Date publicacion_date;
+	private int numcomentarios;
+	private int calificaciones_positivas;
+	private int calificaciones_negativas;
+	private int revisado;
+	private int who_revised;
+
+	public int getId_user() {
+		return id_user;
+	}
+
+	public void setId_user(int id_user) {
+		this.id_user = id_user;
+	}
+
+	public int getVisibilidad() {
+		return visibilidad;
+	}
+
+	public void setVisibilidad(int visibilidad) {
+		this.visibilidad = visibilidad;
+	}
+
+	public String getAsunto() {
+		return asunto;
+	}
+
+	public void setAsunto(String asunto) {
+		this.asunto = asunto;
+	}
+
+	public String getContenido() {
+		return contenido;
+	}
+
+	public void setContenido(String contenido) {
+		this.contenido = contenido;
+	}
+
+	public Date getPublicacion_date() {
+		return publicacion_date;
+	}
+
+	public void setPublicacion_date(Date publicacion_date) {
+		this.publicacion_date = publicacion_date;
+	}
+
+	public int getNumcomentarios() {
+		return numcomentarios;
+	}
+
+	public void setNumcomentarios(int numcomentarios) {
+		this.numcomentarios = numcomentarios;
+	}
+
+	public int getCalificaciones_positivas() {
+		return calificaciones_positivas;
+	}
+
+	public void setCalificaciones_positivas(int calificaciones_positivas) {
+		this.calificaciones_positivas = calificaciones_positivas;
+	}
+
+	public int getCalificaciones_negativas() {
+		return calificaciones_negativas;
+	}
+
+	public void setCalificaciones_negativas(int calificaciones_negativas) {
+		this.calificaciones_negativas = calificaciones_negativas;
+	}
+
+	public int getRevisado() {
+		return revisado;
+	}
+
+	public void setRevisado(int revisado) {
+		this.revisado = revisado;
+	}
+
+	public int getWho_revised() {
+		return who_revised;
+	}
+
+	public void setWho_revised(int who_revised) {
+		this.who_revised = who_revised;
+	}
+
+	public int getIdentificador() {
+		return identificador;
+	}
+
+	public void setIdentificador(int identificador) {
+		this.identificador = identificador;
+	}
+
 }
--- informer-server/informer-api/src/main/java/eetac/upc/edu/dsa/dsaqt1314g2/informer/api/model/PostCollection.java
+++ informer-server/informer-api/src/main/java/eetac/upc/edu/dsa/dsaqt1314g2/informer/api/model/PostCollection.java
@@ -1,8 +1,22 @@
 package eetac.upc.edu.dsa.dsaqt1314g2.informer.api.model;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class PostCollection {
-	
-	
-	
+
+	private List<Post> posts = new ArrayList<Post>();
+
+	public void add(Post post) {
+		posts.add(post);
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 }
