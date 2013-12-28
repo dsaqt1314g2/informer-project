83faf0c76705ac524d771c62235d0ef1a1dd59f7
rodrigo.sampedro <ropnom5291@gmail.com>
2013-12-28 19:26:51 +0100

    añadiendo web de beeter

---
+++ www/js/index.js
@@ -0,0 +1,29 @@
+function Welcome() {
+	var htmlString = '<iframe id="iframe1" src="http://getbootstrap.com/components/" frameborder="0"></iframe>';
+	$('#target_website').html(htmlString);
+}
+
+function Nuevo() {
+	var htmlString = '<iframe id="iframe1" src="new.html" frameborder="0"></iframe>';
+	$('#target_website').html(htmlString);
+}
+
+function Editar() {
+	var htmlString = '<iframe id="iframe1" src="edit.html" frameborder="0"></iframe>';
+	$('#target_website').html(htmlString);
+}
+
+function Get() {
+	var htmlString = '<iframe id="iframe1" src="get.html" frameborder="0"></iframe>';
+	$('#target_website').html(htmlString);
+}
+
+function Delete() {
+	var htmlString = '<iframe id="iframe1" src="delete.html" frameborder="0"></iframe>';
+	$('#target_website').html(htmlString);
+}
+
+function GetLista() {
+	var htmlString = '<iframe id="iframe1" src="lista.html" frameborder="0"></iframe>';
+	$('#target_website').html(htmlString);
+}
