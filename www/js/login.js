2e0e9346e59df9a218e521780226e59540eae963
rodrigo.sampedro <ropnom5291@gmail.com>
2014-01-02 19:21:59 +0100

    tocot c conflictos

---
+++ www/js/login.js
@@ -0,0 +1,36 @@
+
+$("#button_login").click(function(e){
+	e.preventDefault();
+	var username = $('#username').val();
+	var userpass = $('#userpass').val();
+	getLogin(username, userpass);	
+});
+
+function getLogin(username, userpass) {
+	var url = AUTH_BASE_URL + username;
+ 
+	$.ajax({
+		url : url,
+		type : 'GET',
+		crossDomain : true,
+		dataType : 'json',
+		headers : {
+			"Accept" : "application/vnd.informer.api.user+json",
+			//"Access-Control-Allow-Origin" : "*"
+		},
+		beforeSend: function (request)
+	    {
+	        request.withCredentials = true;
+	        request.setRequestHeader("Authorization", "Basic "+ btoa(username+':'+userpass));
+	    },
+	})
+	.done(function (data, status, jqxhr) {
+		$.cookie("username", username);
+		$.cookie("userpass", userpass);
+		window.location = "http://localhost/informer-project/index.html";
+		console.log(data);
+	})
+    .fail(function (jqXHR, textStatus) {
+		console.log(textStatus);
+	});
+}
---
+++ www/js/variable.js
@@ -0,0 +1,2 @@
+var API_BASE_URL = "http://localhost:8000/informer-api";
+var AUTH_BASE_URL = "http://localhost:8000/informer-api/users/";
