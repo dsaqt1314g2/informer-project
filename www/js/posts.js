2be2d2ae6fec85ae181811aca419278e61b3d33b
Raul Suarez <raul.suarez.marin@gmail.com>
2014-01-08 13:05:48 +0100

    subo el post_ranking

--- www/js/posts.js
+++ www/js/posts.js
@@ -3,11 +3,6 @@ var loaded = 0;
 var offset = 1;
 var length = 5;
 
-
-$(document).ready(function(){
-	getListPosts();
-});
-
 function getListPosts() {
 	var url = API_BASE_URL+"posts?o="+offset+"&l="+length;
 	var url = API_BASE_URL+"posts?o="+offset+"&l="+length;
@@ -77,6 +72,64 @@ function getListPosts() {
 	});
 }
 
+function getRankingPosts(ranking) {
+	var url;
+	if (ranking==1) url = API_BASE_URL+"posts/ranking/likes";
+	else if (ranking==2) url = API_BASE_URL+"posts/ranking/dislikes";
+	else if (ranking==3) url = API_BASE_URL+"posts/ranking/coments";
+	else url = API_BASE_URL+"posts?o="+offset+"&l="+length;
+	$.ajax({
+		url : url,
+		type : 'GET',
+		crossDomain : true,
+		dataType : 'json',
+		headers : {
+			"Accept" : "application/vnd.informer.api.post.collection+json",
+		},
+		beforeSend: function (request)
+	    {
+	        request.withCredentials = true;
+	        request.setRequestHeader("Authorization", "Basic "+ btoa('McD0n3ld:McD0n3ld'));
+	    },
+	})
+	.done(function (data, status, jqxhr) {
+		var posts = $.parseJSON(jqxhr.responseText);
+		var htmlString = "<div class='post-container' id='post-container'>";
+		if (loaded > 0) htmlString += document.getElementById('post-container').innerHTML;
+	    $.each(data.posts, function(i,p){	
+	    	htmlString += '<span id="post'+p.identificador+'">';  
+        	htmlString += '<div class="panel panel-primary">';  
+        	htmlString += '<div class="panel-heading"><h3 class="panel-title"><div class="post-autor">'+p.username+' ('+p.identificador+')</div><div class="post-asunto">'+p.asunto+'</div></h3></div>';  
+        	htmlString += '<div class="panel-body">'; 
+        	htmlString += '<div class="post-contenido">'+p.contenido+'</div>';
+			htmlString += '<div class="post-date">Publicado el '+ (new Date(p.publicacion_date)).toLocaleDateString()+' a las '+(new Date(p.publicacion_date)).toLocaleTimeString()+'</div>';
+			if (p.liked == 2)
+				htmlString += '<div class="post-calificaciones_positivas" id="neutro_like'+p.identificador+'"><a href="javascript:void(0);" onClick="processNeutro('+p.identificador+',1)">Ya no me gusta ('+p.calificaciones_positivas+')</a></div>';
+			else
+				htmlString += '<div class="post-calificaciones_positivas" id="like'+p.identificador+'"><a href="javascript:void(0);" onClick="processLike('+p.identificador+',2)">Me gusta ('+p.calificaciones_positivas+')</a></div>';
+			if (p.liked == 1)
+				htmlString += '<div class="post-calificaciones_negativas" id="neutro_dislike'+p.identificador+'"><a href="javascript:void(0);" onClick="processNeutro('+p.identificador+',3)">Ya no es una puta mierda ('+p.calificaciones_negativas+')</a></div>';
+			else
+				htmlString += '<div class="post-calificaciones_negativas" id="dislike'+p.identificador+'"><a href="javascript:void(0);" onClick="processDislike('+p.identificador+',4)">Esto es una puta mierda ('+p.calificaciones_negativas+')</a></div>';
+			htmlString += '<div class="post-denuncia"><a href="javascript:void(0);" id="denuncia'+p.identificador+'" onClick="processDenuncia('+p.identificador+')">Denunciar</a></div>';
+			if (p.numcomentarios == 1)
+				htmlString += '<div class="post-numcomentarios" id="div-num-comentarios'+p.identificador+'"><a href="javascript:void(0);" id="num-comentarios'+p.identificador+'" onClick="processComentarios('+p.identificador+',0)">'+p.numcomentarios+' comentario</a></div>';
+			else
+				htmlString += '<div class="post-numcomentarios" id="div-num-comentarios'+p.identificador+'"><a href="javascript:void(0);" id="num-comentarios'+p.identificador+'" onClick="processComentarios('+p.identificador+',0)">'+p.numcomentarios+' comentarios</a></div>';
+			htmlString += '<div class="post-comentarios-container" id="comentarios-container'+p.identificador+'"></div><br>';
+			htmlString += '<div class="post-mi-comentario-container" id="mi-comentario-container'+p.identificador+'">';
+			htmlString += '			     <textarea class="mi-comentario-txtarea" id="mi-comentario'+p.identificador+'" maxlength=255 spellcheck="false" placeholder="Escribe un comentario..." onkeyup="$(this).css("height","auto");$(this).height(this.scrollHeight);" onkeydown="if (event.keyCode == 13) postComentario('+p.identificador+');"></textarea>';
+			htmlString += '<select id="mi-comentario-visibilidad'+p.identificador+'" style="height: 25px; width: 120px;"><option value="0">Anónimo</option><option value="1">Sólo amigos</option><option value="2">Público</option></select>';
+			htmlString += '			   </div></div></div></span>';
+	    });
+	    htmlString += "</div>";
+		$('#res_get_ranking_posts').html(htmlString);
+		console.log(posts);
+	})
+    .fail(function (jqXHR, textStatus) {
+		console.log(textStatus);
+	});
+}
 
 function postComentario(postid) {
 	var url = API_BASE_URL+"posts/"+postid+"/comentarios";
